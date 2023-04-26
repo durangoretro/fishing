4fa4f25eb0df6f331635861543d940cd27f4113b
Emilio Lopez Berenguer <emilio@emiliollbb.net>
2023-04-26 08:59:09 +0200

    Inception

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
+#include <sprites.h>
+#include "bin/background.h"
+
 int main(void);
 
 int main() {
+    load_background(background);
+    clrscr();
+    
+    
+    while(1){
+    
+    }    
+    
     return 0;
 }
